a422376ef7d05bc0520707fcfcf8536f87354e51
huyhoang462 <22520462@gm.uit.edu.vn>
2024-10-31 14:22:19 +0700

    add folder

---
+++ SE310-Restaurant_Management_System/Controllers/Cashier/CashierController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE310_Restaurant_Management_System.Models;
+
+namespace SE310_Restaurant_Management_System.Controllers.Cashier
+{
+    public class CashierController : Controller
+    {
+        private readonly QlnhaHangContext db;
+        public CashierController(QlnhaHangContext context)
+        {
+            db = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
