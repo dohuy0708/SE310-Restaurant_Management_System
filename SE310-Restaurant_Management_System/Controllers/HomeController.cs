606fe1097243c246c190df2a8f743b04c0dd8b99
votanhoang483 <22520483@gm.uit.edu.vn>
2024-10-31 16:23:16 +0700

    add admin controller, style

--- SE310-Restaurant_Management_System/Controllers/HomeController.cs
+++ SE310-Restaurant_Management_System/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace SE310_Restaurant_Management_System.Controllers
 {
     public class HomeController : Controller
     {
-
+        QlnhaHangContext db = new QlnhaHangContext();
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -16,8 +16,11 @@ namespace SE310_Restaurant_Management_System.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var menuItem = db.MenuItems.ToList();
+            return View(menuItem);
         }
+
+        
 
       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
--- SE310-Restaurant_Management_System/Models/QlnhaHangContext.cs
+++ SE310-Restaurant_Management_System/Models/QlnhaHangContext.cs
@@ -54,8 +54,7 @@ public partial class QlnhaHangContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=LAPTOP-JRRAO9EA\\DOHUY;Initial Catalog=QLNhaHang;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+        => optionsBuilder.UseSqlServer("Data Source=TANHOANG;Initial Catalog=QLNhaHang;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
