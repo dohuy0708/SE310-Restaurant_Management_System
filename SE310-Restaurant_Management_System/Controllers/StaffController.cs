2336924384de01d7792e7f0ff878027ec7ad26aa
huyhoang462 <22520462@gm.uit.edu.vn>
2024-10-31 10:05:46 +0700

    add controller

---
+++ SE310-Restaurant_Management_System/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SE310_Restaurant_Management_System.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
