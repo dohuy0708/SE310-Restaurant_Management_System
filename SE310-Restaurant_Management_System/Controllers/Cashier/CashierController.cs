57d9236547835e9353c9c95df670fe2cb5ec642b
huyhoang462 <22520462@gm.uit.edu.vn>
2024-11-06 09:02:08 +0700

    basic ordering

--- SE310-Restaurant_Management_System/Controllers/Cashier/CashierController.cs
+++ SE310-Restaurant_Management_System/Controllers/Cashier/CashierController.cs
@@ -6,36 +6,85 @@ namespace SE310_Restaurant_Management_System.Controllers.Cashier
 {
     public class CashierController : Controller
     {
-        QlnhaHangContext db = new QlnhaHangContext();
+        private readonly QlnhaHangContext db;
+
+        public CashierController(QlnhaHangContext context)
+        {
+            db = context;
+        }
 
 
-        public IActionResult MenuItem()
+        public IActionResult MenuItem(int? id)
         {
-            var menuItems = db.MenuItems.AsNoTracking().ToList();
-            return View(menuItems);
-        }
-        public IActionResult EmptyTables()
-        {
-            
-            var tables = db.RestaurantTables.AsNoTracking().Where(p => p.StatusId == 2).ToList();
-            return View(tables);
-        }
-        public IActionResult OrderingTables()
-        {
+            // Lấy danh sách SubCategories từ cơ sở dữ liệu
+            var subCategories = db.SubCategories.AsNoTracking().ToList();
 
-            var tables = db.RestaurantTables.AsNoTracking().Where(p => p.StatusId == 1).ToList();
-            return View(tables);
-        }
-        public IActionResult BookingTables()
-        {
+            // Thêm mục "Tất cả" vào đầu danh sách
+            subCategories.Insert(0, new SubCategory
+            {
+                SubCategoryId = 0, // Đặt ID đặc biệt, ví dụ là 0, để đại diện cho "Tất cả"
+                SubCategoryName = "Tất cả"
+            });
 
-            var tables = db.RestaurantTables.AsNoTracking().Where(p => p.StatusId == 3).ToList();
-            return View(tables);
+            // Lấy tất cả menuItems
+            var menuItems = db.MenuItems.AsNoTracking();
+            ViewBag.Name = "Tất cả";
+
+            // Nếu id không null và không phải là 0, lọc menuItems theo SubCategoryId
+            if (id != null && id != 0)
+            {
+                menuItems = menuItems.Where(mi => mi.SubCategoryId == id);
+
+                // Lấy tên SubCategory cho ViewBag.Name nếu có id hợp lệ
+                var subCategory = subCategories.FirstOrDefault(sC => sC.SubCategoryId == id);
+                if (subCategory != null)
+                {
+                    ViewBag.Name = subCategory.SubCategoryName;
+                }
+            }
+
+            ViewBag.SubCategories = subCategories;
+            return View(menuItems.ToList());
         }
+
+        public IActionResult Tables(int? status)
+        {
+            // Lấy danh sách SubCategories từ cơ sở dữ liệu
+            var subCategories = db.SubCategories.AsNoTracking().ToList();
+
+            // Thêm mục "Tất cả" vào đầu danh sách
+            subCategories.Insert(0, new SubCategory
+            {
+                SubCategoryId = 0, // Đặt ID đặc biệt, ví dụ là 0, để đại diện cho "Tất cả"
+                SubCategoryName = "Tất cả"
+            });
+            ViewBag.SubCategories = subCategories;
+
+            var tables = db.RestaurantTables.AsNoTracking();
+            if(status!= null)
+            {
+                tables=tables.Where(t=>t.StatusId==status);
+            }
+            return View(tables.ToList());
+        }
+        public IActionResult GetMenuItems(int? id)
+        {
+            var menuItems = db.MenuItems.AsNoTracking();
+            if (id != null && id != 0)
+            {
+                menuItems = menuItems.Where(mi => mi.SubCategoryId == id);
+            }
+                return PartialView("_MenuItemsPartial", menuItems.ToList());
+        }
+
         public IActionResult BookingOrder()
         {
             var orders = db.BookingOrders.AsNoTracking().ToList();
             return View(orders);
+        }
+        public IActionResult Invoice()
+        {
+            return View();
         }
     }
 }
